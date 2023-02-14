83dcae0e5598461dcf4ce734f3fb67b4660358f1
wusoww <wufujian@szkingdom.com>
2023-02-14 17:34:51 +0800

    FEATURE:1. 优化了DOM结构 2. 兼容PC端 3. 动画效果修复

--- index copy.js
+++ index copy.js
@@ -1,83 +1,227 @@
-// 获取 DOM
-const contentDom = document.querySelector('.content-wrap')
-const loadingDom = document.querySelector('.loading-wrap')
-const listDom = document.querySelector('ul')
+class pullToRefresh {
+    constructor(containerSel, options) {
+        // 初始化容器
+        this.contentDom = document.querySelector(containerSel)
 
-let startX = null
-let startY = null
-let totalScrollTop = 0
-let endHeight = -70
-let touchstartFn = null
-let touchmoveFn = null
-let touchendFn = null
-const maxScrollTop = loadingDom.clientHeight
+        // 保存配置和回调
+        this.options = options
 
-const touchStart = e => {
-    const { clientX, clientY } = e.touches[0]
-    startX = clientX
-    startY = clientY
-    contentDom.style.transition = ''
+        this.initDom()
 
-    contentDom.addEventListener('touchmove', touchMove)
-}
+        this.totalPullNum = 0
+        this.state = null
 
-const touchMove = e => {
-    
-    const { clientX, clientY } = e.touches[0]
 
-    let x = clientX - startX
-    let y = clientY - startY
+        // 初始化loading DOM 的高度, 已下拉值
+        this.headHeight = this.options.headHeight || 40
 
-    let isVerticalScroll = Math.abs(x) < Math.abs(y)
+        // 绑定事件
+        this.bindEvent()
+    }
 
-    if (isVerticalScroll) {
-        // console.log(totalScrollTop, 'totalScrollTop', maxScrollTop, 'maxScrollTop');
-        // console.log('是否纵向滑动');
+    initDom() {
+        // loading DOM 的容器
+        let headDom = document.createElement('div')
+        headDom.className = 'head_dom'
+        headDom.style.backgroundColor = '#fafbfc'
+        headDom.style.overflow = 'hidden'
+        headDom.style.position = 'relative'
+        this.headDom = headDom
 
-        if (totalScrollTop < maxScrollTop) {
-            let scrollHeight = 2
-            if (totalScrollTop > maxScrollTop * 0.5) {
-                scrollHeight = 0.3
-            }
+        // 用户自己的 DOM 的容器
+        let contentDom = document.createElement('div')
+        contentDom.className = 'content_dom'
 
-            if (totalScrollTop > maxScrollTop * 0.9) {
-                loadingDom.innerHTML = '释放立即刷新'
-            }
+        // 把用户的dom 结构 移入到容器里面
+        let children = this.contentDom.children
+        let len = children.length
+        while(len) {
+            contentDom.appendChild(children[0])
+            len--
+        }
 
-            endHeight += scrollHeight
-            totalScrollTop += scrollHeight
+        // 创建 loading DOM
+        let loadingDom = document.createElement('div')
+        loadingDom.innerText = this.options.pullingText
+        loadingDom.style.cssText = `
+            position: absolute;
+            bottom: 0;
+            left: 0;
+            width: 100%;
+            height: ${this.options.headHeight}px;
+            font-size: 12px;
+            text-align: center;
+            line-height: ${this.options.headHeight}px;
+        `
+        this.loadingDom = loadingDom
+        headDom.appendChild(loadingDom)
 
-            contentDom.style.top = endHeight + 'px'
+        // 添加到 根容器
+        this.contentDom.appendChild(headDom)
+        this.contentDom.appendChild(contentDom)
+    }
+
+
+    setState(state) {
+        if (state === 'finish') {
+            this.loadingDom.innerHTML = this.options.completeText
+
+            setTimeout(() => {
+                this.headDom.style.transition = 'height 0.5s'
+                this.headDom.style.height = '0px'
+            }, 1000);
+        }
+
+        if(state === 'failed') {
+            alert('刷新失败, 请重试')
+            this.headDom.style.transition = 'height 0.2s'
+            this.headDom.style.height = '0px'
+        }
+
+        // 重置部分初始值
+        this.totalPullNum = 0
+    }
+
+    bindEvent() {
+        // 判断是否是移动端 或者 PC端
+        let isMobile = this.getUserAgant()
+
+        if (isMobile) {
+            this.bindEventMobile()
+        } else {
+            this.bindEventPc()
         }
     }
 
-    contentDom.addEventListener('touchend', e => {
-        console.log('注册一次');
-        touchEnd()
-    })
+    bindEventMobile() {
+        const touchMove = e => {
+            const { clientX, clientY } = e.touches[0]
+
+            let x = clientX - this.startX
+            let y = clientY - this.startY
+
+            // 是否是纵向滚动
+            let isVerticalScroll = Math.abs(x) < Math.abs(y)
+
+            if (isVerticalScroll) {
+
+                // 简陋的阻力值版本
+                let scrollHeight = 1
+                if (this.totalPullNum > this.headHeight * 0.5 &&
+                    this.totalPullNum < this.headHeight * 2) {
+                    scrollHeight = 0.5
+                } else if (this.totalPullNum > this.headHeight * 2) {
+                    scrollHeight = 0.25
+                }
+
+                if (this.totalPullNum > this.headHeight * 0.8) {
+                    this.loadingDom.innerHTML = this.options.canReleaseText
+                }
+
+                // 计算滚动距离
+                this.totalPullNum += scrollHeight
+
+                this.headDom.style.height = this.totalPullNum + 'px'
+            }
+        }
+
+        const touchEnd = () => {
+            // 当前下拉距离超过 headHeight 需要复原高度
+            if (this.totalPullNum > this.headHeight) {
+                this.headDom.style.transition = 'height 0.2s'
+                this.headDom.style.height = this.headHeight + 'px'
+            }
+            this.loadingDom.innerHTML = this.options.refreshingText
+            this.onRefresh()
+        }
+
+        const touchStart = e => {
+            const { clientX, clientY } = e.touches[0]
+            // 保存触摸点
+            this.startX = clientX
+            this.startY = clientY
+
+            // 清空 headDom transiton  初始化 loadingDom 文本
+            this.loadingDom.innerHTML = this.options.pullingText
+            this.headDom.style.transition = ''
+
+            document.addEventListener('touchmove', touchMove)
+        }
+
+        document.addEventListener('touchstart', touchStart)
+        document.addEventListener('touchend', touchEnd)
+    }
+    
+    bindEventPc() {
+        this.contentDom.style.userSelect = 'none'
+        const dragStart = e => {
+            const { pageY } = e
+            // 保存触摸点
+            this.startY = pageY
+
+            // 清空 headDom transiton  初始化 loadingDom 文本
+            this.loadingDom.innerHTML = this.options.pullingText
+            this.headDom.style.transition = ''
+
+            document.addEventListener('mousemove', dragMove)
+        }
+
+        const dragMove = e => {
+            const { pageY } = e
+
+            let y = pageY - this.startY
+
+            // 是否是纵向滚动
+            let isVerticalScroll = y > 0
+
+            if (isVerticalScroll) {
+
+                // 简陋的阻力值版本
+                let scrollHeight = 1
+                if (this.totalPullNum > this.headHeight * 0.5 &&
+                    this.totalPullNum < this.headHeight * 2) {
+                    scrollHeight = 0.5
+                } else if (this.totalPullNum > this.headHeight * 2) {
+                    scrollHeight = 0.25
+                }
+
+                if (this.totalPullNum > this.headHeight * 0.8) {
+                    this.loadingDom.innerHTML = this.options.canReleaseText
+                }
+
+                // 计算滚动距离
+                this.totalPullNum += scrollHeight
+
+                this.headDom.style.height = this.totalPullNum + 'px'
+            }
+        }
+
+        const dragEnd = e => {
+            document.removeEventListener('mousemove', dragMove)
+            // 当前下拉距离超过 headHeight 需要复原高度
+            if (this.totalPullNum > this.headHeight) {
+                this.headDom.style.transition = 'height 0.2s'
+                this.headDom.style.height = this.headHeight + 'px'
+            }
+
+            // 有效滚动的情况下才去执行回调
+            if (this.totalPullNum) {
+               this.loadingDom.innerHTML = this.options.refreshingText
+                this.onRefresh() 
+            }
+        }
+
+
+
+        document.addEventListener('mousedown', dragStart)
+        document.addEventListener('mouseup', dragEnd)
+    }
+
+    onRefresh() {}
+
+    getUserAgant() {
+        return navigator.userAgent.includes('Mobile') ||
+               navigator.userAgent.includes('Android') ||
+               navigator.userAgent.includes('iPhone')
+    }
 }
-
-const touchEnd = () => {
-    loadingDom.innerHTML = '加载中'
-    setTimeout(() => {
-        loadingDom.innerHTML = '刷新成功'
-
-        setTimeout(() => {
-            contentDom.style.transition = 'top 0.5s'
-            contentDom.style.top = '-70px'
-
-            // contentDom.removeEventListener('touchstart', touchstartFn)
-            contentDom.removeEventListener('touchmove', touchMove)
-            contentDom.removeEventListener('touchend', touchEnd)
-            totalScrollTop = 0
-            endHeight = -70
-        }, 500)
-    }, 1000)
-}
-
-function init() {
-    touchstartFn = touchStart.bind(this, ...arguments)
-    contentDom.addEventListener('touchstart', touchstartFn)
-}
-
-init()
--- index.js
+++ index.js
@@ -1,128 +1,186 @@
 class pullToRefresh {
     constructor(containerSel, options) {
-        // 获取容器 DOM
+        // 初始化容器
         this.contentDom = document.querySelector(containerSel)
+
         // 保存配置和回调
         this.options = options
-        // this.onRefresh = cb
-        this.totalScrollTop = 0
+
+        // 初始化 DOM 结构
+        this.initDom()
+
+        this.totalPullNum = 0
         this.state = null
 
-        // 创建loading DOM
-        this.creatLoadingDom()
-
-        // 初始化最大下拉值, 已下拉值
-        this.maxScrollNum = this.loadingDom.clientHeight
-        this.scrolledNum = -this.maxScrollNum
+        // 初始化loading DOM 的高度, 已下拉值
+        this.headHeight = this.options.headHeight || 40
 
         // 绑定事件
         this.bindEvent()
     }
-    setState(state) {
-        if (state == 'pending') {
-            this.loadingDom.innerHTML = this.options.refreshingText
-            this.contentDom.style.transition = 'top 0.5s'
-            this.contentDom.style.top = '-70px'
 
-            // this.contentDom.removeEventListener('touchmove', touchMove)
-
-            // 重置部分初始值
-            this.totalScrollTop = 0
-            this.scrolledNum = -this.maxScrollNum
-        }
-    }
-
-    creatLoadingDom() {
-        let div = document.createElement('div')
-        div.style.cssText = `
+    initDom() {
+        // 创建loading DOM 的容器
+        let headDom = document.createElement('div')
+        headDom.className = 'head_dom'
+        headDom.style.cssText = `
             position: relative;
+            overflow: hidden;
+            background-color: #fafbfc;
+        `
+        this.headDom = headDom
+
+        // 创建用户自己的 DOM 的容器
+        let contentDom = document.createElement('div')
+        contentDom.className = 'content_dom'
+
+        // 把用户的dom 移入到容器里面
+        let children = this.contentDom.children
+        let len = children.length
+        while (len) {
+            contentDom.appendChild(children[0])
+            len--
+        }
+
+        // 创建 loading DOM
+        let loadingDom = document.createElement('div')
+        loadingDom.innerText = this.options.pullingText
+        loadingDom.style.cssText = `
+            position: absolute;
+            bottom: 0;
+            left: 0;
             width: 100%;
-            height: 70px;
+            height: ${this.options.headHeight}px;
             font-size: 12px;
             text-align: center;
-            line-height: 70px;
-            background-color: #fafbfc;  
+            line-height: ${this.options.headHeight}px;
         `
-        div.innerText = this.options.pullingText
-        this.contentDom.insertBefore(div, this.contentDom.children[0])
-        this.loadingDom = this.contentDom.children[0]
+        this.loadingDom = loadingDom
+        headDom.appendChild(loadingDom)
+
+        // 添加到 根容器
+        this.contentDom.appendChild(headDom)
+        this.contentDom.appendChild(contentDom)
+    }
+
+
+    setState(state) {
+        if (state === 'finish') {
+            this.loadingDom.innerHTML = this.options.completeText
+
+            setTimeout(() => {
+                this.headDom.style.transition = 'height 0.5s'
+                this.headDom.style.height = '0px'
+            }, 1000);
+        }
+
+        if (state === 'failed') {
+            alert('刷新失败, 请重试')
+            this.headDom.style.transition = 'height 0.2s'
+            this.headDom.style.height = '0px'
+        }
+
+        // 重置部分初始值
+        this.totalPullNum = 0
     }
 
     bindEvent() {
+        // 判断是否是移动端 或者 PC端
+        let isMobile = this.getUserAgant()
+
+        // 初始化事件名称
+        let startEventName = isMobile ? 'touchstart' : 'mousedown'
+        let endEventName = isMobile ? 'touchend' : 'mouseup'
+        let moveEventName = isMobile ? 'touchmove' : 'mousemove'
+
+
+        if (!isMobile) this.contentDom.style.userSelect = 'none'
+
         const touchMove = e => {
-            const { clientX, clientY } = e.touches[0]
+            let isVerticalScroll
 
-            let x = clientX - this.startX
-            let y = clientY - this.startY
+            if (isMobile) {
+                const { clientX, clientY } = e.touches[0]
 
-            // 是否是纵向滚动
-            let isVerticalScroll = Math.abs(x) < Math.abs(y)
+                let x = clientX - this.startX
+                let y = clientY - this.startY
 
-            if (isVerticalScroll) {
-                // 总下拉值 是否 小于 边界值
-                if (this.totalScrollTop < this.maxScrollNum) {
+                isVerticalScroll = Math.abs(x) < Math.abs(y)
+            } else {
+                const { pageY } = e
 
-                    // 简陋的阻力值版本
-                    let scrollHeight = 2
-                    if (this.totalScrollTop > this.maxScrollNum * 0.5) {
-                        scrollHeight = 0.3
-                    }
+                let y = pageY - this.startY
 
-                    if (this.totalScrollTop > this.maxScrollNum * 0.8) {
-                        this.loadingDom.innerHTML = this.options.canReleaseText
-                    }
-
-                    // 计算滚动距离
-                    this.scrolledNum += scrollHeight
-                    this.totalScrollTop += scrollHeight
-
-                    this.contentDom.style.top = this.scrolledNum + 'px'
-                }
+                isVerticalScroll = y > 0
             }
 
+
+            if (isVerticalScroll) {
+
+                // 简陋的阻力值版本
+                let scrollHeight = 1
+                if (this.totalPullNum > this.headHeight * 0.5 &&
+                    this.totalPullNum < this.headHeight * 2) {
+                    scrollHeight = 0.5
+                } else if (this.totalPullNum > this.headHeight * 2) {
+                    scrollHeight = 0.25
+                }
+
+                if (this.totalPullNum > this.headHeight * 0.8) {
+                    this.loadingDom.innerHTML = this.options.canReleaseText
+                }
+
+                // 计算滚动距离
+                this.totalPullNum += scrollHeight
+
+                this.headDom.style.height = this.totalPullNum + 'px'
+            }
         }
 
         const touchEnd = () => {
-            this.loadingDom.innerHTML = this.options.refreshingText
+            // PC端松开后要解绑move事件 要不然会触发滚动
+            if (!isMobile) document.removeEventListener('mousemove', touchMove)
 
-            // setTimeout(() => {
-            //     this.loadingDom.innerHTML = this.options.completeText
-        
-            //     setTimeout(() => {
-            //         this.contentDom.style.transition = 'top 0.5s'
-            //         this.contentDom.style.top = '-70px'
-        
-            //         this.contentDom.removeEventListener('touchmove', touchMove)
+            // 当前下拉距离超过 headHeight 需要复原高度
+            if (this.totalPullNum > this.headHeight) {
+                this.headDom.style.transition = 'height 0.2s'
+                this.headDom.style.height = this.headHeight + 'px'
+            }
 
-            //         // 重置部分初始值
-            //         this.totalScrollTop = 0
-            //         this.scrolledNum = -this.maxScrollNum
-            //     }, 500)
-            // }, 1000)
-            this.onPre()
-            this.onRefresh()
+            // 有效滚动的情况下才去执行回调
+            if (this.totalPullNum) {
+                this.loadingDom.innerHTML = this.options.refreshingText
+                this.onRefresh()
+            }
         }
 
         const touchStart = e => {
-            const { clientX, clientY } = e.touches[0]
-            // 保存触摸点
-            this.startX = clientX
-            this.startY = clientY
-            // 清空上次设置的过渡, 避免第二次下拉的时候 过渡生效
-            this.contentDom.style.transition = ''
-            this.loadingDom.innerHTML = this.options.pullingText
+            if (isMobile) {
+                const { clientX, clientY } = e.touches[0]
+                this.startX = clientX
+                this.startY = clientY
+            } else {
+                const { pageY } = e
+                this.startY = pageY
+            }
 
-            document.addEventListener('touchmove', touchMove)
+
+            // 清空 headDom transiton  初始化 loadingDom 文本
+            this.loadingDom.innerHTML = this.options.pullingText
+            this.headDom.style.transition = ''
+
+            document.addEventListener(moveEventName, touchMove)
         }
 
-        document.addEventListener('touchstart', touchStart)
-        document.addEventListener('touchend', touchEnd)
+        document.addEventListener(startEventName, touchStart)
+        document.addEventListener(endEventName, touchEnd)
     }
 
-    onRefresh() {
+    onRefresh() { }
 
-    }
-
-    onPre() {
+    getUserAgant() {
+        return navigator.userAgent.includes('Mobile') ||
+            navigator.userAgent.includes('Android') ||
+            navigator.userAgent.includes('iPhone')
     }
 }
