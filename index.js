ff9e772279e1672d67bb724978ccd7e4c679f35c
wusoww <wufujian@szkingdom.com>
2023-02-15 22:53:48 +0800

    FIX: 构造函数传参优化

--- index.js
+++ index.js
@@ -1,194 +1,199 @@
 class commonDrag {
-    constructor(sel, options) {
-        this.dragDom = document.querySelector(sel)
+	// 参数问题
 
-        this.dragDom.style.userSelect = 'none'
-        this.dragDom.style.position = 'relative'
+	// 阻力系数
+	constructor(sel, options) {
+		this.dragDom = document.querySelector(sel)
 
-        this.options = options
+		this.dragDom.style.userSelect = 'none'
+		this.dragDom.style.position = 'relative'
 
-        this.startPos = {}
+		this.options = options
 
-        if (this.options.bindEventImmediately) {
-            this.bindEvent()
-        }
-    }
+		this.dragDirection = this.options.dragDirection || 'none'
 
-    bindEvent() {
-        // 执行 开始拖拽前 钩子函数
-        this.beforeDrag()
-        // 判断是否是移动端 或者 PC端
-        let isMobile = this.isMobile()
+		this.startPos = {}
 
-        // 初始化事件名称
-        let startEventName = isMobile ? 'touchstart' : 'mousedown'
-        let moveEventName = isMobile ? 'touchmove' : 'mousemove'
-        let endEventName = isMobile ? 'touchend' : 'mouseup'
+		// if (this.options.bindEventImmediately) {
+		this.bindEvent()
+		// }
+	}
 
-        const dragStart = e => {
-            // 执行 拖拽开始 钩子函数
-            this.onDragStart()
+	bindEvent() {
+		// 判断是否是移动端 或者 PC端
+		let isMobile = this.isMobile()
 
-            // 如果限制了拖拽范围, 计算边界值
-            if (this.options.containerClass) this.calculateDragRange()
+		// 初始化事件名称
+		let startEventName = isMobile ? 'touchstart' : 'mousedown'
+		let moveEventName = isMobile ? 'touchmove' : 'mousemove'
+		let endEventName = isMobile ? 'touchend' : 'mouseup'
 
-            // 记录下第一次点击时候的坐标
-            if (isMobile) {
-                const { clientX, clientY } = e.touches[0]
+		const dragStart = e => {
+			// 执行 开始拖拽前 钩子函数
+			this.beforeDrag()
+			// 执行 拖拽开始 钩子函数
+			this.onDragStart()
 
-                let x = clientX
-                let y = clientY
+			// 如果限制了拖拽范围, 计算边界值
+			if (this.options.containerClass) this.calculateDragRange()
 
-                if (this.options.containerClass) {
-                    x = x - this.graph.offsetLeft - this.dragDom.offsetLeft
-                    y = y - this.graph.offsetTop - this.dragDom.offsetTop
-                }
+			// 记录下第一次点击时候的坐标
+			if (isMobile) {
+				const { clientX, clientY } = e.touches[0]
 
-                this.startPos = {
-                    x,
-                    y
-                }
-            } else {
-                const { pageX, pageY } = e
+				let x = clientX
+				let y = clientY
 
-                let x = pageX
-                let y = pageY
+				if (this.options.containerClass) {
+					x = x - this.graph.offsetLeft - this.dragDom.offsetLeft
+					y = y - this.graph.offsetTop - this.dragDom.offsetTop
+				}
 
-                if (this.options.containerClass) {
-                    x = x - this.graph.offsetLeft - this.dragDom.offsetLeft
-                    y = y - this.graph.offsetTop - this.dragDom.offsetTop
-                }
+				this.startPos = {
+					x,
+					y
+				}
+			} else {
+				const { pageX, pageY } = e
 
-                this.startPos = {
-                    x,
-                    y
-                }
-            }
+				let x = pageX
+				let y = pageY
 
-            // 添加 move 事件
-            document.addEventListener(moveEventName, dragMove)
-        }
+				if (this.options.containerClass) {
+					x = x - this.graph.offsetLeft - this.dragDom.offsetLeft
+					y = y - this.graph.offsetTop - this.dragDom.offsetTop
+				}
 
-        const dragMove = e => {
-            let nowPos
+				this.startPos = {
+					x,
+					y
+				}
+			}
 
-            if (isMobile) {
-                const { clientX, clientY } = e.touches[0]
+			// 添加 move 事件
+			document.addEventListener(moveEventName, dragMove)
+		}
 
-                nowPos = {
-                    x: clientX,
-                    y: clientY
-                }
-            } else {
-                const { pageX, pageY } = e
+		const dragMove = e => {
+			let nowPos
 
-                nowPos = {
-                    x: pageX,
-                    y: pageY
-                }
+			if (isMobile) {
+				const { clientX, clientY } = e.touches[0]
 
-            }
+				nowPos = {
+					x: clientX,
+					y: clientY
+				}
+			} else {
+				const { pageX, pageY } = e
 
-            let direction = this.calculateDirection(nowPos)
+				nowPos = {
+					x: pageX,
+					y: pageY
+				}
 
-            if (this.options.dragDirection == 'none' || direction === this.options.dragDirection) {
-                this.calculateMove(nowPos)
-            }
-        }
+			}
 
-        const dragEnd = e => {
-            // 执行 拖拽结束 钩子函数
-            this.onDragEnd()
+			let direction = this.calculateDirection(nowPos)
 
-            // 解绑事件
-            document.removeEventListener(moveEventName, dragMove)
-        }
+			if (this.dragDirection == 'none' || direction === this.dragDirection) {
+				this.calculateMove(nowPos)
+			}
+		}
 
-        this.dragDom.addEventListener(startEventName, dragStart)
-        document.addEventListener(endEventName, dragEnd)
-    }
+		const dragEnd = e => {
+			// 执行 拖拽结束 钩子函数
+			this.onDragEnd()
 
-    calculateDragRange() {
-        if (!this.options.containerClass) return
+			// 解绑事件
+			document.removeEventListener(moveEventName, dragMove)
+		}
 
-        let graph = document.querySelector(this.options.containerClass)
-        graph.style.position = 'relative'
-        this.graph = graph
+		this.dragDom.addEventListener(startEventName, dragStart)
+		document.addEventListener(endEventName, dragEnd)
+	}
 
-        let graphWidth = graph.clientWidth
-        let graphHeight = graph.clientHeight
+	calculateDragRange() {
+		if (!this.options.containerClass) return
 
-        const dragWidth = this.dragDom.clientWidth
-        const dragHeight = this.dragDom.clientHeight
+		let graph = document.querySelector(this.options.containerClass)
+		graph.style.position = 'relative'
+		this.graph = graph
 
-        this.maxX = graphWidth - dragWidth
-        this.maxY = graphHeight - dragHeight
-    }
+		let graphWidth = graph.clientWidth
+		let graphHeight = graph.clientHeight
 
-    calculateDirection(endPos) {
-        if (this.options.containerClass) {
-            const w = this.dragDom.offsetWidth
-            const h = this.dragDom.offsetHeight
-            var x = (endPos.x - this.dragDom.offsetLeft - (w / 2)) * (w > h ? (h / w) : 1),
-                y = (endPos.y - this.dragDom.offsetTop - (h / 2)) * (h > w ? (w / h) : 1),
-                // 上(0) 右(1) 下(2) 左(3)
-                direction = Math.round((((Math.atan2(y, x) * (180 / Math.PI)) + 180) / 90) + 3) % 4
+		const dragWidth = this.dragDom.clientWidth
+		const dragHeight = this.dragDom.clientHeight
 
-            return ['up', 'right', 'down', 'left'][direction]
-        } else {
-            const { x, y } = this.startPos
+		this.maxX = graphWidth - dragWidth
+		this.maxY = graphHeight - dragHeight
+	}
 
-            // if (endPos.x - x > 0) {
-            //     return 'right'
-            // } else if (endPos.x - x < 0) {
-            //     return 'left'
-            // } else 
-            if (endPos.y - y > 0) {
-                return 'down'
-            } else if (endPos.y - y < 0) {
-                return 'up'
-            }
-        }
+	calculateDirection(endPos) {
+		if (this.options.containerClass) {
+			const w = this.dragDom.offsetWidth
+			const h = this.dragDom.offsetHeight
+			var x = (endPos.x - this.dragDom.offsetLeft - (w / 2)) * (w > h ? (h / w) : 1),
+				y = (endPos.y - this.dragDom.offsetTop - (h / 2)) * (h > w ? (w / h) : 1),
+				// 上(0) 右(1) 下(2) 左(3)
+				direction = Math.round((((Math.atan2(y, x) * (180 / Math.PI)) + 180) / 90) + 3) % 4
 
-    }
+			return ['up', 'right', 'down', 'left'][direction]
+		} else {
+			const { x, y } = this.startPos
 
-    calculateMove(nowPos) {
-        let x = nowPos.x - this.startPos.x
-        let y = nowPos.y - this.startPos.y
+			// if (endPos.x - x > 0) {
+			//     return 'right'
+			// } else if (endPos.x - x < 0) {
+			//     return 'left'
+			// } else 
+			if (endPos.y - y > 0) {
+				return 'down'
+			} else if (endPos.y - y < 0) {
+				return 'up'
+			}
+		}
 
-        // 拖拽是否限制范围
-        if (this.options.containerClass) {
-            x = x - this.graph.offsetLeft
-            y = y - this.graph.offsetTop
+	}
 
-            // 边界值处理
-            if (x < 0) x = 0
-            if (y < 0) y = 0
+	calculateMove(nowPos) {
+		let x = nowPos.x - this.startPos.x
+		let y = nowPos.y - this.startPos.y
 
-            if (x > this.maxX) x = this.maxX
-            if (y > this.maxY) y = this.maxY
-        }
-        // 执行 拖拽中 钩子函数
-        if (this.options.customMoveFn) {
-            this.onDragMove(x, y)
-            return
-        }
+		// 拖拽是否限制范围
+		if (this.options.containerClass) {
+			x = x - this.graph.offsetLeft
+			y = y - this.graph.offsetTop
 
-        this.dragDom.style.top = y + 'px'
-        this.dragDom.style.left = x + 'px'
-    }
+			// 边界值处理
+			if (x < 0) x = 0
+			if (y < 0) y = 0
 
-    isMobile() {
-        return navigator.userAgent.includes('Mobile') ||
-            navigator.userAgent.includes('Android') ||
-            navigator.userAgent.includes('iPhone')
-    }
+			if (x > this.maxX) x = this.maxX
+			if (y > this.maxY) y = this.maxY
+		}
+		// 执行 拖拽中 钩子函数
+		if (this.options.customMoveFn) {
+			this.onDragMove(x, y)
+			return
+		}
 
-    beforeDrag() { }
+		this.dragDom.style.top = y + 'px'
+		this.dragDom.style.left = x + 'px'
+	}
 
-    onDragStart() { }
+	isMobile() {
+		return navigator.userAgent.includes('Mobile') ||
+			navigator.userAgent.includes('Android') ||
+			navigator.userAgent.includes('iPhone')
+	}
 
-    onDragMove() { }
+	beforeDrag() { }
 
-    onDragEnd() { }
+	onDragStart() { }
+
+	onDragMove() { }
+
+	onDragEnd() { }
 }
